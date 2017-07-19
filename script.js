207688ffe7472a6bcbc591877a603775a9ab828f
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-07-19 17:41:58 +0200

    exercise_1

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-var span = $("span");
-    span.each(function(index, element) {
-});
-$("span:even").css('color', 'red');
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
-
-});
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+var arr = ["def", "qas", "wsx"];
+jQuery.each( arr, function( i, val ) {
+  $( "#" + val ).css({"color" : "blue"});
 });
