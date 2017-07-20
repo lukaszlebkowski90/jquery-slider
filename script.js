1e4359db44a04f97c69f81ab7c6ed2ae5a67d7a8
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-07-20 13:50:23 +0200

    update

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
-	setInterval(function(){
-		$("#carousel ul").animate({marginLeft:-480},500,function(){
-			$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});
-		})
-	},1000);
+var carousel = $("#carousel ul");
+var liLast = carousel.find("li:last");
+var liFirst = carousel.find("li:first");
+function slider_right () {
+	carousel.animate({marginLeft:-480},500,function(){
+		var liLast = carousel.find("li:last");
+		var liFirst = carousel.find("li:first");			
+		liLast.after(liFirst);
+		carousel.css({marginLeft:0});
+	})
+};
+// setInterval(slider_right, 1000);
+function slider_left () {
+	carousel.animate({marginLeft: 0}, 500, function(){
+		var liLast = carousel.find("li:last");
+		var liFirst = carousel.find("li:first");
+		liFirst.before(liLast);
+		carousel.css({marginLeft:-480});
+	});
+};
+// setInterval(slider_left, 1000);
+$("#arrow-next").on("click", function() {
+	slider_right ();
 });
+$("#arrow-prev").on("click", function() {
+	slider_left();
+});
+
 
 
