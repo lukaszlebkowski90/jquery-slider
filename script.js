8e8d0132dd04f796c7d997898613e3fcb805f5a9
Łukasz Łebkowski <lukasz.lebkowski90@gmail.com>
2017-07-30 15:57:15 +0200

    dsa

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-var p = $("p");
-  p.each(function(index, element)  {
-    var id = p.attr("id");
-    var ignored = ["qas", "mok", "pla"];
+$("p").each(function(index, value)  {
+    var id = $(this).attr("id");
+    var ignored = ["abc", "mok", "pla", "wsx"];
     var cssObject = {color:"red"};
     var indexOfElement = ignored.indexOf(id);
       if (indexOfElement === -1) {
+        $(value).css(cssObject);
       }  
   });
-  
-
-
